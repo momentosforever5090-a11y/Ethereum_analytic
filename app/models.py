23eada811290920aa1a4a1ad701c994d211b49b7
momentosforever5090-a11y <momentosforever5090@gmail.com>
2026-04-27 16:34:30 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from flask_login import UserMixin
 
+class User(UserMixin):
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+# Base de datos simulada (puedes pasarla a config más adelante)
+users_db = {
+    'admin': {'password': 'user'},
+    'user': {'password': 'pass'}
+}
